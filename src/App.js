69b90a59056e2bc465f5a046d61264fc5a0af2c0
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-04 19:51:50 -0400

    Added toDoList array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import ToDoDisplay from './components/TodoComponents/TodoList';
+
+const toDoList = [
+  {
+      display: "Learn setState()",
+      isChecked: false
+  }, 
+  {
+      display: "Style my Todo List",
+      isChecked: false
+  }
+]
 
 class App extends React.Component {
   constructor() {
