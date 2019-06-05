b9bdbf2ced43a59abc476806394237238c8e0afe
sandyjernigan <sandy.jernigan@gmail.com>
2019-06-04 20:04:01 -0400

    Added components to display

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoDisplay />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ToDoForm from './components/TodoComponents/TodoForm';
-import ToDoItem from './components/TodoComponents/Todo';
+import ToDoForm from './TodoForm';
+import ToDoItem from './Todo';
 
 // This Displays the To Do List -- imports the individual todo items and the form
 
 const ToDoDisplay = () => {
     return (
       <div class="todolist">
+          <ToDoForm />
+          <ToDoItem />
       </div>
     );
 }
